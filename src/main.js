dbd26c275373ff6516d565885f4433ae996b039d
Daniel Pacheco <Daniel.Pacheco@Truecommerce.com>
2019-05-28 13:12:29 -0400

    Adding loader

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import { firestorePlugin } from 'vuefire'
 import VueRouter from 'vue-router'
+import NProgress from 'nprogress';
 import App from './App.vue'
 import { MdApp, MdToolbar, MdMenu, MdButton, MdContent, MdTabs, MdIcon, MdList, MdCard, MdDrawer } from '../node_modules/vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css'
@@ -11,6 +12,8 @@ import AddPet from './components/AddPet.vue'
 import EditPet from './components/EditPet.vue'
 import ListPet from './components/ListPet.vue'
 import Home from './components/Home.vue'
+
+import '../node_modules/nprogress/nprogress.css'
 
 Vue.use(MdApp)
 Vue.use(MdToolbar)
@@ -50,6 +53,17 @@ const routes = [
 ];
 
 const router = new VueRouter({ mode: 'history', routes: routes });
+
+router.beforeResolve((to, from, next) => {
+  if (to.name) {
+      NProgress.start()
+  }
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done()
+})
 
 new Vue({
   render: h => h(App),
